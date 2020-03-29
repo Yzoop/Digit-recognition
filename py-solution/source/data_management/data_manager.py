940abc9a47a7af20dcc530fcd22fa39ae8991c4d
Ivan Kalinchuk <iv.ikalin@gmail.com>
2020-03-29 22:33:16 +0300

    i dont know

--- py-solution/source/data_management/data_manager.py
+++ py-solution/source/data_management/data_manager.py
@@ -3,7 +3,7 @@ import os
 #scipy for .mat file
 
 # save name of a file with data
-__mat_file_name__ = 'data_management/data/digit_data.mat'
+__mat_file_name__ = 'data_management/digit_data.mat'
 
 def get_training_data():
     """
@@ -12,6 +12,7 @@ def get_training_data():
     Description: This function is used for reading and taking training data
                  (digit images)
     """
+    print(os.getcwd())
     training_data = loadmat(__mat_file_name__)
 
     return training_data
