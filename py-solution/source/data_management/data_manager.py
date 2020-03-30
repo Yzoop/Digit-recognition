ce35427d0ef8479e90aba22ed359d4864a1990e2
Ivan Kalinchuk <iv.ikalin@gmail.com>
2020-03-30 09:44:17 +0300

    changes for csv

--- py-solution/source/data_management/data_manager.py
+++ py-solution/source/data_management/data_manager.py
@@ -1,9 +1,9 @@
-from scipy.io import loadmat
+from numpy import genfromtxt
 import os
 #scipy for .mat file
 
 # save name of a file with data
-__mat_file_name__ = 'data_management/digit_data.mat'
+TRAINING_DATA = 'data_management/training_data.csv'
 
 def get_training_data():
     """
@@ -13,6 +13,6 @@ def get_training_data():
                  (digit images)
     """
     print(os.getcwd())
-    training_data = loadmat(__mat_file_name__)
+    training_data = genfromtxt(TRAINING_DATA,delimiter=',')
 
     return training_data
